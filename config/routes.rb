7fd236c54f6d063cd64af7f4e02a65a38be83318
Steve Morrissey <uberamd@gmail.com>
2017-09-22 15:35:36 -0500

    adding a logout route, and joining

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
 
-  get 'sessions/logout'
 
-  # this is where steam login returns back to
+  # SESSION ROUTES
+  ## this is where steam login returns back to
   post '/auth/steam/callback', to: 'sessions#create'
+  ## logout
+  get '/logout', to: 'sessions#logout'
 
   # team routes
   resources :teams
+  get '/teams/:id/join', to: 'teams#join_request', as: 'join_team'
+  get '/teams/:id/join/:user/approve', to: 'teams#join_approve', as: 'approve_join_team'
 
   root to: 'static_page#index'
 end
