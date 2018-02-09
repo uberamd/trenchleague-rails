2e7add7589e9cb2b9c8930a87ef7c2b9aa1a8b56
Steve Morrissey <uberamd@gmail.com>
2018-02-09 11:50:24 -0600

    fixing redis session store logic

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,12 +1,27 @@
-TrenchleagueRails::Application.config.session_store :redis_store, {
-    servers: [
-        {
-            host: "localhost",
-            port: 16379,
-            db: 0,
-            namespace: "session"
-        },
-    ],
-    expire_after: 90.minutes,
-    key: "_#{Rails.application.class.parent_name.downcase}_session"
-}
+if ENV['RAILS_ENV'] == 'production'
+  TrenchleagueRails::Application.config.session_store :redis_store, {
+      servers: [
+          {
+              host: "cache",
+              port: 6379,
+              db: 0,
+              namespace: "session"
+          },
+      ],
+      expire_after: 30.days,
+      key: "_#{Rails.application.class.parent_name.downcase}_session"
+  }
+else
+  TrenchleagueRails::Application.config.session_store :redis_store, {
+      servers: [
+          {
+              host: "localhost",
+              port: 16379,
+              db: 0,
+              namespace: "session"
+          },
+      ],
+      expire_after: 30.days,
+      key: "_#{Rails.application.class.parent_name.downcase}_session"
+  }
+end
