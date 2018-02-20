ce41f12ba4e43d244453a177c3a3e405679cc3e2
Steve Morrissey <uberamd@gmail.com>
2018-02-19 22:55:20 -0600

    updating job with user tracking

--- app/jobs/opendota_mmr_refresh_job.rb
+++ app/jobs/opendota_mmr_refresh_job.rb
@@ -4,11 +4,17 @@ class OpendotaMmrRefreshJob < ApplicationJob
   queue_as :default
 
   def perform(user)
+    # set some raven contexts
+    Raven.user_context(id: user.id)
+
     # get the data from the opendota API
     response = HTTParty.get("https://api.opendota.com/api/players/#{user.get_short_steamid}/")
 
     # parse the response JSON
     api_hash = JSON.parse(response.body)
+
+    # add the hash to raven
+    Raven.extra_context(api_hash: api_hash)
 
     # if solo MMR exists, update it
     if api_hash['solo_competitive_rank'] != nil
@@ -33,10 +39,12 @@ class OpendotaMmrRefreshJob < ApplicationJob
     user.opendota_mmr_updated = Time.now()
 
     # at least for now, update other fields as well
-    user.personaname = api_hash['profile']['personaname']
-    user.profileurl = api_hash['profile']['profileurl']
-    user.avatar = api_hash['profile']['avatarmedium']
-    user.loccountrycode = api_hash['profile']['loccountrycode']
+    if api_hash['profile']
+      user.personaname = api_hash['profile']['personaname']
+      user.profileurl = api_hash['profile']['profileurl']
+      user.avatar = api_hash['profile']['avatarmedium']
+      user.loccountrycode = api_hash['profile']['loccountrycode']
+    end
 
     user.save!
   end
