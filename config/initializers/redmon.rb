3d278b42a9a9123e808f0a9ae050f00ae4390c32
Steve Morrissey <smorrissey@olson.com>
2018-05-23 10:47:54 -0500

    adding password

--- config/initializers/redmon.rb
+++ config/initializers/redmon.rb
@@ -6,7 +6,6 @@ require 'redmon/app'
 # Optional config overrides
 #
 Redmon.configure do |config|
-  config.redis_url = ENV['REDIS_uri']
+  config.redis_url = "redis://:#{ENV['REDIS_password']}@redis:6379"
   config.namespace = 'redmon'
-  config.password = ENV['REDIS_password']
 end
