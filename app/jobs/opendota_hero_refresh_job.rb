6fe0d951c61361ec0aa4e42eb6d0824fa0ff8011
Steve Morrissey <smorrissey@olson.com>
2018-05-24 14:31:09 -0500

    addresses job issue in #2

--- app/jobs/opendota_hero_refresh_job.rb
+++ app/jobs/opendota_hero_refresh_job.rb
@@ -7,26 +7,34 @@ class OpendotaHeroRefreshJob < ApplicationJob
     # get the data from the opendota API
     response = HTTParty.get("https://api.opendota.com/api/players/#{user.get_short_steamid}/heroes")
 
-    # parse the response JSON
-    api_hash = JSON.parse(response.body)
+    if response.code != 200
+      # something went wrong
+      Raven.capture_message("Error running job, non 200", {
+          :tags => {'job' => 'opendota_hero_refresh'}})
+    else
 
-    api_hash.each do |hero|
-      # find the real hero id from the db by referencing the opendota provided ID
-      real_hero = Hero.find_by(odota_hero_id: hero['hero_id'].to_i)
+      # parse the response JSON
+      api_hash = JSON.parse(response.body)
 
-      # create or find an entry for that user and the corresponding hero
-      tmp_user_hero = UserHeroes.find_or_create_by(user_id: user.id, hero_id: real_hero.id)
+      api_hash.each do |hero|
+        # find the real hero id from the db by referencing the opendota provided ID
+        real_hero = Hero.find_by(odota_hero_id: hero['hero_id'].to_i)
 
-      # finally update the wins and losses
-      tmp_user_hero.update_attributes({
-          last_played: hero['last_played'],
-          games: hero['games'],
-          win: hero['win'],
-          with_games: hero['with_games'],
-          with_win: hero['with_win'],
-          against_games: hero['against_games'],
-          against_win: hero['against_win']
-                                      })
+        # create or find an entry for that user and the corresponding hero
+        tmp_user_hero = UserHeroes.find_or_create_by(user_id: user.id, hero_id: real_hero.id)
+
+        # finally update the wins and losses
+        tmp_user_hero.update_attributes({
+                                            last_played: hero['last_played'],
+                                            games: hero['games'],
+                                            win: hero['win'],
+                                            with_games: hero['with_games'],
+                                            with_win: hero['with_win'],
+                                            against_games: hero['against_games'],
+                                            against_win: hero['against_win']
+                                        })
+      end
     end
+
   end
 end
