712be3ffb436938b08aba58633e95c7963817698
Steve Morrissey <smorrissey@olson.com>
2018-05-25 08:20:44 -0500

    sanitizing

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :steam, ENV['STEAM_WEB_API_KEY']
-  provider :steam, '711E7AAD4D8935151C9C11B29DCC03C2'
+  provider :steam, ENV['STEAM_WEB_API_KEY']
 end
